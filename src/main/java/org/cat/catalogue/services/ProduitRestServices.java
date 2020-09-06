8fadc408646667ebbb450ae3d5b20a26b17b2c2a
Amraoui Youssef <amraoui.joseph@gmail.com>
2020-09-07 00:27:14 +0200

    APIRest

--- src/main/java/org/cat/catalogue/services/ProduitRestServices.java
+++ src/main/java/org/cat/catalogue/services/ProduitRestServices.java
@@ -4,9 +4,7 @@ import org.cat.catalogue.dao.ProduitRepository;
 import org.cat.catalogue.entities.Produit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
 import java.util.List;
@@ -20,8 +18,21 @@ public class ProduitRestServices {
     public List<Produit> getListProduits(){
         return produitRepository.findAll();
     }
-    @GetMapping(value = "/Produit/{id}")
+    @GetMapping(value = "/produit/{id}")
     public Produit getProduit(@PathVariable(name="id") Long id){
         return produitRepository.findById(id).get();
+    }
+    @PutMapping(value = "/updateProduit/{id}")
+    public Produit updateProduit(@PathVariable(name = "id") Long id,@RequestBody Produit p){
+        p.setId(id);
+        return produitRepository.save(p);
+    }
+    @PostMapping(value = "/saveProduit")
+    public Produit saveProduit(@RequestBody Produit p){
+        return produitRepository.save(p);
+    }
+    @DeleteMapping(value = "deleteProduit/{id}")
+    public void deleteProduit(@PathVariable(name = "id") Long id){
+        produitRepository.deleteById(id);
     }
 }
